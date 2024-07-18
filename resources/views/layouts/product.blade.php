a6dca2dbc227cedc30e24c358b5754ab44285296
fatima594 <fatimalkhl33@gmail.com>
2024-07-18 14:52:43 +0330

    modified products file view

--- resources/views/layouts/product.blade.php
+++ resources/views/layouts/product.blade.php
@@ -25,7 +25,7 @@
                 <div class="col-lg-4 col-md-6 text-center">
                     <div class="single-product-item">
                         <div class="product-image">
-                            <a href="single-product.html"><img src="{{ asset('storage/' . $product->image) }}" alt="{{ $product->name }}" style="max-width: 100px; max-height: 100px;"></a>
+                            <a href="single-product.html"><img src="{{ asset('storage/' . $product->image) }}" alt="{{ $product->name }}" style="max-width: 300px; max-height: 300px !important;"></a>
                         </div>
                         <br>
                         <h3>{{ $product->name }}</h3>
@@ -35,7 +35,10 @@
 
                         <a href="cart.html" class="cart-btn alert-warning"><i class="fas fa-shopping-cart"></i> Add to Cart</a>
                     </div>
+                    <br>
+                    <br>
                 </div>
+
                 @endforeach
 
             </div>
